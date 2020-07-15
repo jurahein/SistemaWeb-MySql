8b77408663a50344fa910160994e9ee286776d7b
Juraci <jurahein@gmail.com>
2020-07-14 22:35:01 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,9 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Minha aplicação Web";
+            ViewData["Autor"] = "Juraci Hein Silva Magalhães";
+            ViewData["Email"] = "jurahein@gmail.com";
 
             return View();
         }
